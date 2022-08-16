7fafafecfb87a9243b575b13fd2e5f412d0e2ccf
aprhong90 <bboshsh.sy@gmail.com>
2022-08-15 21:12:51 -0400

    created React.js app with TimeConverter component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,51 @@
-import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
 
+function TimeConverter() {
+  const [timeAmount, setTimeAmount] = useState();
+  const [flipped, setFlipped] = useState(false);
+
+  const reset = (e) => {
+    e.preventDefault();
+    setTimeAmount(0);
+  };
+  const onFlip = (e) => {
+    e.preventDefault();
+    reset(e);
+    setFlipped((curr) => !curr);
+  };
+  return (
+    <form>
+      <label htmlFor="minutes">Minutes</label>
+      <input
+        type="number"
+        name="minutes"
+        id="minutes"
+        value={flipped ? timeAmount * 60 : timeAmount}
+        onChange={(e) => setTimeAmount(e.target.value)}
+        disabled={flipped}
+      />
+      <label htmlFor="hours">Hours</label>
+      <input
+        type="number"
+        name="hours"
+        id="hours"
+        value={!flipped ? Math.round(timeAmount / 60) : timeAmount}
+        disabled={!flipped}
+        onChange={(e) => setTimeAmount(e.target.value)}
+      />
+      <button onClick={reset}>reset</button>
+      <button onClick={onFlip}>Flip</button>
+    </form>
+  );
+}
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Super Converter</h1>
+      <TimeConverter />
+    </div>
+  );
 }
 
 export default App;
